e4b0cf1ec13b98cdfbcc160d88b5b88621a9bfaa
Nikita-rajpoot <139115321+Nikita-rajpoot@users.noreply.github.com>
2023-07-18 14:07:34 +0530

    Cake.js

--- src/Components/Cake.js
+++ src/Components/Cake.js
@@ -3,132 +3,135 @@ import React from "react";
 function Cake() {
   return (
     <div ClassName="cake">
-      <h4 className="text-info">cake</h4>
-      <div className="grid-container">
-        <div className="grid-item">
-          <img
-            src="/images/donate1.jpg"
-            className="d-block w-100"
-            height="300px"
-            width="80px"
-            border="1"
-          />
-          <p>Name$$$</p>
+     <section className="cake bg-light">
+        <div className="container ">
+          <div className="row py-5 text-center">
+            <h2>Cake</h2>
+            <div className="col-lg-6 m-auto">
+              <div className="line my-4"></div>
+              <p>
+                Welcome to The Bakery Shoppe. We offer whole cakes, individual
+                slices of cake, pastries, cupcakes, muffins, and pies. We also
+                offer a wide variety of hot and cold beverages such as Lattes,
+                cappuccino, coffee, tea, soda, juice, and many more. We have
+                seating available.
+              </p>
+            </div>
+          </div>
+          <div className="row py-5">
+            <div className="col-lg-4">
+              <div className="card p-2">
+                <div className="card-body">
+                  <img
+                    src="./images/cake9.jpg"
+                    className="img-fluid pb-3"
+                    alt="Cake"
+                    width="400px"
+                  />
+                  <h5 className="head1">Wow Black Forest</h5>
+                  <p className="per1">Black Forest flavored cake 
+                  Chocolate Dropping and Topping With Cherry Round Shape</p>
+                  <h5 className="head1">Rs. 600</h5>
+                  <button className="btnc mt-4">Add to Cart</button>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="card">
+                <div className="card-body">
+                  <img
+                    src="./images/c1.jpg"
+                    className="img-fluid pb-3"
+                    alt="Cake"
+                    width="400px"
+                  />
+                  <h5 className="head1">Heavenly Rose Cake</h5>
+                  <p className="per1">Cream Round Shape Whipped cream Birthdays Anniversary </p>
+                  <h5 className="head1">Rs. 799</h5>
+                  <button className="btnc mt-4">Add to Cart</button>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="card">
+                <div className="card-body">
+                  <img
+                    src="./images/Home3.jpg"
+                    className="img-fluid pb-3"
+                    alt="Cake"
+                    width="400px"
+                  />
+                  <h5 className="head1">Fluffy Flaky Vanilla Cake</h5>
+                  <p className="per1">This delicious cake consists of: Half KG Vanilla cake White rose on top
+                   Vanilla flavor Whipped cream Suitable for</p>
+                  <h5 className="head1">Rs. 750</h5>
+                  <button className="btnc mt-4">Add to Cart</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br></br>
+          <div className="row py-5">
+            <div className="col-lg-4">
+              <div className="card">
+                <div className="card-body">
+                  <img
+                    src="./images/cake8.jpg"
+                    className="img-fluid pb-3"
+                    alt="Cake"
+                    width="400px"
+                  />
+                  <h5 className="head1">Chocolate Drizzled Cake</h5>
+                  <p className="per1">Type of Sponge: Chocolate 
+                  Toppings: Cream, Chocolate flakes & Coffee dust Garnish </p>
+                  <h5 className="head1">Rs. 550</h5>
+                  <button className="btnc mt-4">Add to Cart</button>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="card">
+                <div className="card-body">
+                  <img
+                    src="./images/c2.jfif"
+                    className="img-fluid pb-3"
+                    alt="Cake"
+                    width="400px"
+                  />
+                  <h5 className="head1">Toothsome Chocolate Cake</h5>
+                  <p className="per1">Type of Sponge: Chocolate</p>
+                  <h5 className="head1">Rs.599</h5>
+                  <button className="btnc mt-4">Add to Cart</button>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="card">
+                <div className="card-body">
+                  <img
+                    src="./images/c5.jfif"
+                    className="img-fluid pb-3"
+                    alt="Cake"
+                    width="400px"
+                  />
+                  <h5 className="head1">Creme Rose Cake</h5>
+                  <p className="per1">Type of Cream: Vanilla </p>
+                  <h5 className="head1">Rs. 600</h5>
+                  <button className="btnc mt-4">Add to Cart</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="grid-item">
-          <img
-            src="/images/donate2.jpg"
-            className="d-block w-100"
-            height="300px"
-            width="100%"
-            border="1"
-          />
-          <p>Name$$$</p>
-          <p>Macarons Around a Pink Cake</p>
-        </div>
-        <div className="grid-item">
-          <img
-            src="/images/donate3.jpg"
-            className="d-block w-100"
-            height="300px"
-            width="100%"
-            border="1"
-          />
-          <p>Name$$$</p>
-        </div>
-        <div className="grid-item">
-          <img
-            src="/images/donate4.jpg"
-            className="d-block w-100"
-            height="300px"
-            width="100%"
-            border="1"
-          />
-          <p>Name$$$</p>
-        </div>
-        <div className="grid-item">
-          <img
-            src="/images/cake6.jfif"
-            className="d-block w-100"
-            height="300px"
-            width="100%"
-            border="1"
-          />
-          <p>Name$$$</p>
-        </div>
-        <div className="grid-item">
-          <img
-            src="/images/cake6.jpg"
-            className="d-block w-100"
-            height="300px"
-            width="100%"
-            border="1"
-          />
-          <p>Name$$$</p>
-        </div>
-        <div className="grid-item">
-          <img
-            src="/images/cake7.jpg"
-            className="d-block w-100"
-            height="300px"
-            width="100%"
-            border="1"
-          />
-          <p>Name$$$</p>
-        </div>
-        <div className="grid-item">
-          <img
-            src="/images/cake8.jpg"
-            className="d-block w-100"
-            height="300px"
-            width="100%"
-            border="1"
-          />
-          <p>Name$$$</p>
-        </div>
-        <div className="grid-item">
-          <img
-            src="/images/cake9.jpg"
-            className="d-block w-100"
-            height="300px"
-            width="100%"
-            border="1"
-          />
-          <p>Name$$$</p>
-        </div>
-        <div className="grid-item">
-          <img
-            src="/images/cake10.jpg"
-            className="d-block w-100"
-            height="300px"
-            width="100%"
-            border="1"
-          />
-          <p>Name$$$</p>
-        </div>
-        <div className="grid-item">
-          <img
-            src="/images/cake11.png"
-            className="d-block w-100"
-            height="300px"
-            width="100%"
-            border="1"
-          />
-          <p>Name$$$</p>
-        </div>
-        <div className="grid-item">
-          <img
-            src="/images/cake12.jfif"
-            className="d-block w-100"
-            height="300px"
-            width="100%"
-            border="1"
-          />
-          <p>Name$$$</p>
-        </div>
-        <br></br>
-      </div>
+      </section>
     </div>
   );
+       
+         
+            
+            
+          
+         
+          
 }
 export default Cake;
